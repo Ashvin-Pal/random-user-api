65e8aa8a18e45e7cbaf0d6202c75ea2f6f932dd9
Ashvin-Pal <ashvinshash@gmail.com>
2022-06-09 23:41:10 +0200

    feat(test): add mocks for unit tests

---
+++ mocks/UsersResponse.tsx
@@ -0,0 +1,412 @@
+import { RandomUserResponse } from "../types";
+
+export const mockResponse: RandomUserResponse = {
+    results: [
+        {
+            gender: "female",
+            name: {
+                title: "Mademoiselle",
+                first: "Éveline",
+                last: "Renaud",
+            },
+            location: {
+                street: {
+                    number: 2954,
+                    name: "Rue Abel-Ferry",
+                },
+                city: "Isenthal",
+                state: "Nidwalden",
+                country: "Switzerland",
+                postcode: 5247,
+                coordinates: {
+                    latitude: "-53.7175",
+                    longitude: "178.8475",
+                },
+                timezone: {
+                    offset: "+2:00",
+                    description: "Kaliningrad, South Africa",
+                },
+            },
+            email: "eveline.renaud@example.com",
+            dob: {
+                date: "1952-10-19T15:59:48.907Z",
+                age: 70,
+            },
+            id: {
+                name: "AVS",
+                value: "756.6455.7891.12",
+            },
+            picture: {
+                large: "https://randomuser.me/api/portraits/women/45.jpg",
+                medium: "https://randomuser.me/api/portraits/med/women/45.jpg",
+                thumbnail: "https://randomuser.me/api/portraits/thumb/women/45.jpg",
+            },
+        },
+        {
+            gender: "male",
+            name: {
+                title: "Mr",
+                first: "Saban",
+                last: "Dippel",
+            },
+            location: {
+                street: {
+                    number: 2357,
+                    name: "Königsberger Straße",
+                },
+                city: "Dannenberg (Elbe)",
+                state: "Schleswig-Holstein",
+                country: "Germany",
+                postcode: 15342,
+                coordinates: {
+                    latitude: "27.7698",
+                    longitude: "0.7115",
+                },
+                timezone: {
+                    offset: "+1:00",
+                    description: "Brussels, Copenhagen, Madrid, Paris",
+                },
+            },
+            email: "saban.dippel@example.com",
+            dob: {
+                date: "1955-06-28T05:22:36.806Z",
+                age: 67,
+            },
+            id: {
+                name: "",
+                value: "12",
+            },
+            picture: {
+                large: "https://randomuser.me/api/portraits/men/16.jpg",
+                medium: "https://randomuser.me/api/portraits/med/men/16.jpg",
+                thumbnail: "https://randomuser.me/api/portraits/thumb/men/16.jpg",
+            },
+        },
+        {
+            gender: "female",
+            name: {
+                title: "Mrs",
+                first: "Jeannine",
+                last: "Glatz",
+            },
+            location: {
+                street: {
+                    number: 8359,
+                    name: "Wiesenstraße",
+                },
+                city: "Rendsburg",
+                state: "Sachsen-Anhalt",
+                country: "Germany",
+                postcode: 70274,
+                coordinates: {
+                    latitude: "-53.2472",
+                    longitude: "111.5399",
+                },
+                timezone: {
+                    offset: "+5:45",
+                    description: "Kathmandu",
+                },
+            },
+            email: "jeannine.glatz@example.com",
+            dob: {
+                date: "1996-10-27T01:00:00.738Z",
+                age: 26,
+            },
+            id: {
+                name: "",
+                value: "jn",
+            },
+            picture: {
+                large: "https://randomuser.me/api/portraits/women/21.jpg",
+                medium: "https://randomuser.me/api/portraits/med/women/21.jpg",
+                thumbnail: "https://randomuser.me/api/portraits/thumb/women/21.jpg",
+            },
+        },
+        {
+            gender: "female",
+            name: {
+                title: "Ms",
+                first: "Sara",
+                last: "Cano",
+            },
+            location: {
+                street: {
+                    number: 2575,
+                    name: "Avenida de Salamanca",
+                },
+                city: "Bilbao",
+                state: "Galicia",
+                country: "Spain",
+                postcode: 14133,
+                coordinates: {
+                    latitude: "0.1766",
+                    longitude: "-92.6593",
+                },
+                timezone: {
+                    offset: "-10:00",
+                    description: "Hawaii",
+                },
+            },
+            email: "sara.cano@example.com",
+            dob: {
+                date: "1950-09-02T22:05:25.286Z",
+                age: 72,
+            },
+            id: {
+                name: "DNI",
+                value: "96745867-F",
+            },
+            picture: {
+                large: "https://randomuser.me/api/portraits/women/16.jpg",
+                medium: "https://randomuser.me/api/portraits/med/women/16.jpg",
+                thumbnail: "https://randomuser.me/api/portraits/thumb/women/16.jpg",
+            },
+        },
+        {
+            gender: "male",
+            name: {
+                title: "Mr",
+                first: "Leo",
+                last: "Mason",
+            },
+            location: {
+                street: {
+                    number: 1734,
+                    name: "Prospect Rd",
+                },
+                city: "Philadelphia",
+                state: "Ohio",
+                country: "United States",
+                postcode: 30463,
+                coordinates: {
+                    latitude: "-43.4146",
+                    longitude: "16.8253",
+                },
+                timezone: {
+                    offset: "+4:30",
+                    description: "Kabul",
+                },
+            },
+            email: "leo.mason@example.com",
+            dob: {
+                date: "1944-10-22T12:30:51.258Z",
+                age: 78,
+            },
+            id: {
+                name: "SSN",
+                value: "334-50-0767",
+            },
+            picture: {
+                large: "https://randomuser.me/api/portraits/men/76.jpg",
+                medium: "https://randomuser.me/api/portraits/med/men/76.jpg",
+                thumbnail: "https://randomuser.me/api/portraits/thumb/men/76.jpg",
+            },
+        },
+        {
+            gender: "male",
+            name: {
+                title: "Mr",
+                first: "Tom",
+                last: "Bertrand",
+            },
+            location: {
+                street: {
+                    number: 1660,
+                    name: "Rue Courbet",
+                },
+                city: "Argenteuil",
+                state: "Indre-et-Loire",
+                country: "France",
+                postcode: 79802,
+                coordinates: {
+                    latitude: "26.2548",
+                    longitude: "-117.3985",
+                },
+                timezone: {
+                    offset: "-12:00",
+                    description: "Eniwetok, Kwajalein",
+                },
+            },
+            email: "tom.bertrand@example.com",
+            dob: {
+                date: "1953-02-12T08:11:35.836Z",
+                age: 69,
+            },
+            id: {
+                name: "INSEE",
+                value: "1NNaN08841346 12",
+            },
+            picture: {
+                large: "https://randomuser.me/api/portraits/men/14.jpg",
+                medium: "https://randomuser.me/api/portraits/med/men/14.jpg",
+                thumbnail: "https://randomuser.me/api/portraits/thumb/men/14.jpg",
+            },
+        },
+        {
+            gender: "male",
+            name: {
+                title: "Mr",
+                first: "Marcus",
+                last: "Thomas",
+            },
+            location: {
+                street: {
+                    number: 7917,
+                    name: "Victoria Street",
+                },
+                city: "New Plymouth",
+                state: "Otago",
+                country: "New Zealand",
+                postcode: 87144,
+                coordinates: {
+                    latitude: "28.8551",
+                    longitude: "63.9458",
+                },
+                timezone: {
+                    offset: "-8:00",
+                    description: "Pacific Time (US & Canada)",
+                },
+            },
+            email: "marcus.thomas@example.com",
+            dob: {
+                date: "1981-11-06T03:46:22.477Z",
+                age: 41,
+            },
+            id: {
+                name: "",
+                value: "hbh",
+            },
+            picture: {
+                large: "https://randomuser.me/api/portraits/men/50.jpg",
+                medium: "https://randomuser.me/api/portraits/med/men/50.jpg",
+                thumbnail: "https://randomuser.me/api/portraits/thumb/men/50.jpg",
+            },
+        },
+        {
+            gender: "female",
+            name: {
+                title: "Mrs",
+                first: "Tammy",
+                last: "Reyes",
+            },
+            location: {
+                street: {
+                    number: 2652,
+                    name: "O'Connell Avenue",
+                },
+                city: "Castlebar",
+                state: "Longford",
+                country: "Ireland",
+                postcode: 43493,
+                coordinates: {
+                    latitude: "-78.1423",
+                    longitude: "-174.9622",
+                },
+                timezone: {
+                    offset: "-3:30",
+                    description: "Newfoundland",
+                },
+            },
+            email: "tammy.reyes@example.com",
+            dob: {
+                date: "1948-07-07T11:41:06.638Z",
+                age: 74,
+            },
+            id: {
+                name: "PPS",
+                value: "3396925T",
+            },
+            picture: {
+                large: "https://randomuser.me/api/portraits/women/41.jpg",
+                medium: "https://randomuser.me/api/portraits/med/women/41.jpg",
+                thumbnail: "https://randomuser.me/api/portraits/thumb/women/41.jpg",
+            },
+        },
+        {
+            gender: "female",
+            name: {
+                title: "Ms",
+                first: "Hannah",
+                last: "Miller",
+            },
+            location: {
+                street: {
+                    number: 5743,
+                    name: "Richmond Ave",
+                },
+                city: "Chipman",
+                state: "Prince Edward Island",
+                country: "Canada",
+                postcode: 78,
+                coordinates: {
+                    latitude: "48.6557",
+                    longitude: "172.0242",
+                },
+                timezone: {
+                    offset: "-1:00",
+                    description: "Azores, Cape Verde Islands",
+                },
+            },
+            email: "hannah.miller@example.com",
+            dob: {
+                date: "1990-04-10T11:04:50.649Z",
+                age: 32,
+            },
+            id: {
+                name: "",
+                value: "78",
+            },
+            picture: {
+                large: "https://randomuser.me/api/portraits/women/81.jpg",
+                medium: "https://randomuser.me/api/portraits/med/women/81.jpg",
+                thumbnail: "https://randomuser.me/api/portraits/thumb/women/81.jpg",
+            },
+        },
+        {
+            gender: "male",
+            name: {
+                title: "Mr",
+                first: "Jayden",
+                last: "Andersen",
+            },
+            location: {
+                street: {
+                    number: 3659,
+                    name: "3rd St",
+                },
+                city: "Grand Falls",
+                state: "Northwest Territories",
+                country: "Canada",
+                postcode: "N4N 0O7",
+                coordinates: {
+                    latitude: "-29.7530",
+                    longitude: "130.4735",
+                },
+                timezone: {
+                    offset: "-3:00",
+                    description: "Brazil, Buenos Aires, Georgetown",
+                },
+            },
+            email: "jayden.andersen@example.com",
+            dob: {
+                date: "1997-07-18T19:01:14.392Z",
+                age: 25,
+            },
+            id: {
+                name: "",
+                value: "90",
+            },
+            picture: {
+                large: "https://randomuser.me/api/portraits/men/56.jpg",
+                medium: "https://randomuser.me/api/portraits/med/men/56.jpg",
+                thumbnail: "https://randomuser.me/api/portraits/thumb/men/56.jpg",
+            },
+        },
+    ],
+    info: {
+        seed: "2becc1fcdceb2e14",
+        results: 10,
+        page: 1,
+        version: "1.3",
+    },
+};
